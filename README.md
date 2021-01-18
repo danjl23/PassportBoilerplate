06c44bbf8d7ceb0b6e186d8fb0828113589ec728
Daniel Lawson <danlawson23@gmail.com>
2021-01-19 06:38:19 +1000

    Initial

---
+++ README.md
@@ -0,0 +1,61 @@
+# Passport.js Boilerplate
+An Express App which allows users to create accounts, and login with Passport.js authentication. Accounts & Sessions are stored in MongoDB.
+
+## To-Do List
+- [x] Reset Password System
+- [x] Store Session in MongoDB
+- [x] reCAPTCHA
+- [ ] Account Activation **(working on it!)**
+- [ ] Change Account Details
+- [ ] CSRF Protection **(working on it!)**
+
+## Dependencies
+https://www.npmjs.com/package/express  
+https://www.npmjs.com/package/connect-flash  
+https://www.npmjs.com/package/dotenv  
+https://www.npmjs.com/package/ejs  
+https://www.npmjs.com/package/express-session  
+https://www.npmjs.com/package/mongoose  
+https://www.npmjs.com/package/connect-mongo  
+https://www.npmjs.com/package/nodemailer  
+https://www.npmjs.com/package/passport  
+https://www.npmjs.com/package/passport-local  
+https://www.npmjs.com/package/passport-local-mongoose  
+https://www.npmjs.com/package/express-recaptcha  
+
+## Configuration
+```sh
+$ cp .env.template .env
+```
+### Example:
+```
+# Server Port
+PORT = 3000
+
+# MongoDB Connection String
+MONGO_URI = 'mongodb+srv://example:abc123@cluster.tgr8u.mongodb.net/users?retryWrites=true&w=majority'
+
+# Session (Expiry in Milliseconds)
+SECRET = 'sessionsecret'
+EXPIRY = 1800000
+
+# Email Settings
+SMTP_HOST = 'smtp.mailtrap.io'
+SMTP_PORT = 2525
+SMTP_USER = 'yourusername'
+SMTP_PASS = 'yourpassword'
+SMTP_FROM = 'email@example.com'
+
+# reCAPTCHA (v2 Checkbox)
+SITE_KEY = 'Get from https://www.google.com/recaptcha/admin'
+SECRET_KEY = 'Get from https://www.google.com/recaptcha/admin'
+```  
+## Usage
+```sh
+$ npm i
+```
+```sh
+$ npm run dev
+# OR
+$ npm run start
+```
---
+++ app.js
@@ -0,0 +1,84 @@
+// Require
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const flash = require('connect-flash')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+
+const app = express()
+
+// Models
+const User = require('./models/userModel')
+
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+})
+.then(() => console.log('MongoDB Connection Established'))
+.catch(error => console.error(error))
+
+// Middleware - EJS
+app.set('view engine', 'ejs')
+
+// Middleware - BodyParser
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// Middleware - Session
+app.use(session({
+    secret: process.env.SECRET,
+    resave: true,
+    saveUninitialized: true,
+    // MongoDB Session
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    cookie: { maxAge: Number(process.env.EXPIRY) }
+}))
+
+// Middleware - Flash
+app.use(flash())
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg')
+    res.locals.error_msg = req.flash('error_msg')
+    // Passport Error
+    res.locals.error = req.flash('error')
+    next()
+})
+
+// Middleware - Passport
+app.use(passport.initialize())
+app.use(passport.session())
+
+// Passport
+passport.use(new LocalStrategy({ usernameField: 'email', passwordField: 'pass' }, User.authenticate()))
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
+// Global Variables
+app.use((req, res, next) => {
+    res.locals.currentUser = req.user
+    res.locals.session = req.session
+    next()
+})
+
+// Check if Authenticated
+module.exports = {
+    requireAuth: function(req, res, next) {
+        if(req.isAuthenticated()) {
+            return next()
+        }
+        req.flash('error_msg', 'You are not logged in!')
+        res.redirect('/login')
+    }
+}
+
+// Routes
+app.use(require('./routes/indexRoutes'))
+app.use(require('./routes/authRoutes'))
+
+// Listen on Port
+app.listen(process.env.PORT, console.log(`Server Running on Port ${process.env.PORT}`))
